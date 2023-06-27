c42cc32bd32919a15207e4fe52321c4d3c51eb4b
Hakim Barraud <hakim.barraud@gmail.com>
2023-06-27 16:19:01 +0200

    Create the Search Button

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,15 +1,23 @@
 import Input from "./Input";
 import Way from "./Way";
 import { FaLocationDot } from "react-icons/fa6";
+import { BsRecordCircleFill } from "react-icons/bs";
 
 const SearchBar = () => {
   return (
     <form>
       <Way />
       <Input
-        placeholder="From: City, Region, Airport"
+        placeholder="From: City, Station Or Airport"
+        icon={<BsRecordCircleFill />}
+      />
+      <Input
+        placeholder="To: City, Station Or Airport"
         icon={<FaLocationDot />}
       />
+      <button className="w-full bg-red-400 text-white p-3 mt-2 rounded-xl font-bold">
+        Search
+      </button>
     </form>
   );
 };
