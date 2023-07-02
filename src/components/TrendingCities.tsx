7f00785b292c503358ccec10c6ca2c63a3497a86
Hakim Barraud <hakim.barraud@gmail.com>
2023-07-02 19:40:43 +0200

    Add comments to the TrendingCities component

--- src/components/TrendingCities.tsx
+++ src/components/TrendingCities.tsx
@@ -2,12 +2,13 @@ import { Fragment } from "react";
 import usePopularCities from "../Hooks/usePopularCities";
 import Loader from "./Loader";
 
+// Declare the shape of the props that this component accepts
 interface Props {
-  setInput?: (text: string) => () => void;
+  setInput?: (text: string) => () => void; // Set the value of the input to the item selected in the list displayed
 }
 
 const TrendingCities = ({ setInput }: Props) => {
-  const { data, isLoading } = usePopularCities();
+  const { data, isLoading } = usePopularCities(); // Call the usePopularCities custom hook and destructure the returned data and isLoading variables
 
   return (
     <div className="px-12 bg-white border-2 border-black rounded-lg border-opacity-10 p-2 mt-1 shadow-lg absolute w-full z-10">
