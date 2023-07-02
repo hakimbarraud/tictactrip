a11ccab95a93a363c6911237fa49dee98ee89a54
Hakim Barraud <hakim.barraud@gmail.com>
2023-07-02 19:37:26 +0200

    Add comments into the DestinationOptions component

--- src/components/DestinationOptions.tsx
+++ src/components/DestinationOptions.tsx
@@ -2,18 +2,16 @@ import { Fragment } from "react";
 import useDestinationCities from "../Hooks/useDestinationCities";
 import Loader from "./Loader";
 
+// Define the shape of the props of the component
 interface Props {
-  setDestination?: (text: string) => () => void;
-  city: string;
+  setDestination?: (text: string) => () => void; // Function to set the value of the input to the city selected in the list of popular destinations
+  city: string; // value of the text set in the first input (used to display the popular destinations)
 }
 
-const DestinationOptions = ({
-  setDestination,
-  city,
-}: Pick<Props, "setDestination" | "city">) => {
-  const { data, isLoading } = useDestinationCities(city);
+const DestinationOptions = ({ setDestination, city }: Props) => {
+  const { data, isLoading } = useDestinationCities(city); // Fetch the data and loading state of the useDestinationCities custom hook
 
-  if (!city) return null;
+  if (!city) return null; // Hide the component if no value has been provided in the first input field
 
   return (
     <div className="px-12 bg-white border-2 border-black rounded-lg border-opacity-10 p-2 mt-1 shadow-lg absolute w-full z-10">
