458e6c9c21abebd670948999e7b7c414eb0c3161
Hakim Barraud <hakim.barraud@gmail.com>
2023-07-02 19:31:19 +0200

    Add comments into the AutocompletResults component

--- src/components/AutocompleteResults.tsx
+++ src/components/AutocompleteResults.tsx
@@ -2,16 +2,14 @@ import { Fragment } from "react";
 import useAutocomplete from "../Hooks/useAutocomplete";
 import Loader from "./Loader";
 
+// Define the shape of the props expected by the component
 interface Props {
-  setInput?: (text: string) => () => void;
-  text: string;
+  setInput?: (text: string) => () => void; // Function to set the value of the input to the city selected in the list of autocompletion
+  text: string; // City selected in the first input (it's used to display the most popular destinations from this city)
 }
 
-const AutocompleteResults = ({
-  setInput,
-  text,
-}: Pick<Props, "setInput" | "text">) => {
-  const { data, isLoading } = useAutocomplete(text);
+const AutocompleteResults = ({ setInput, text }: Props) => {
+  const { data, isLoading } = useAutocomplete(text); // Call the useAutocomplete custom hook to fetch autocomplete data based on the input text
 
   return (
     <div className="px-12 bg-white border-2 border-black rounded-lg border-opacity-10 p-2 mt-1 shadow-lg absolute w-full z-10">
